02b5fa7345c04ed600e1334a3a18cf53c877beee
wuhengyu <578060214@qq.com>
2023-12-19 14:18:54 +0800

    files update

---
+++ src/router.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, computed } from "vue";
+import Home from "./Pages/Home.vue";
+import About from "./Pages/About.vue";
+import NotFound from "./Pages/NotFound.vue";
+import App from "./Pages/Foundation/App.vue";
+import calculate from "./Pages/Foundation/calculate.vue";
+const routes = {
+  "/": Home,
+  "/about": About,
+  "/App": App,
+  "/calculate": calculate,
+  "/non-existent-path": NotFound,
+};
+const currentPath = ref(window.location.hash);
+window.addEventListener("hashchange", () => {
+  currentPath.value = window.location.hash;
+});
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || "/"] || NotFound;
+});
+</script>
+<template>
+  <a href="#/">Home</a> | <a href="#/about">About</a> |
+  <a href="#/App">App</a> | <a href="#/calculate">calculate</a> |
+  <a href="#/non-existent-path">NotFound</a>
+  <component :is="currentView" />
+</template>
