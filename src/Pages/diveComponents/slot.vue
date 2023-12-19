3b15b751ca38210dac08549accdad6ccf5ad7ec7
wuhengyu <578060214@qq.com>
2023-12-19 17:31:53 +0800

    files update

---
+++ src/Pages/diveComponents/slot.vue
@@ -0,0 +1,13 @@
+<script setup>
+import FancyButton from "./FancyButton.vue";
+import AwesomeIcon from "./AwesomeIcon.vue";
+</script>
+
+<template>
+  <br />
+  <FancyButton> Click me </FancyButton>
+  <FancyButton>
+    <span style="color: cyan">Click me! </span>
+    <AwesomeIcon />
+  </FancyButton>
+</template>
--- src/router.vue
+++ src/router.vue
@@ -5,12 +5,14 @@ import About from "./Pages/About.vue";
 import NotFound from "./Pages/NotFound.vue";
 import App from "./Pages/Foundation/App.vue";
 import calculate from "./Pages/Foundation/calculate.vue";
+import slot from "./Pages/diveComponents/slot.vue";
 const routes = {
   "/": Home,
   "/about": About,
   "/App": App,
   "/calculate": calculate,
   "/non-existent-path": NotFound,
+  "/slot": slot,
 };
 const currentPath = ref(window.location.hash);
 window.addEventListener("hashchange", () => {
@@ -23,6 +25,6 @@ const currentView = computed(() => {
 <template>
   <a href="#/">Home</a> | <a href="#/about">About</a> |
   <a href="#/App">App</a> | <a href="#/calculate">calculate</a> |
-  <a href="#/non-existent-path">NotFound</a>
+  <a href="#/non-existent-path">NotFound</a> | <a href="#/slot">slot</a>
   <component :is="currentView" />
 </template>
