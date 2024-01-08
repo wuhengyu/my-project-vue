a67fa785e5236267fd40449593e90f6cc5a05d08
吴恒宇 <578060214@qq.com>
2024-01-08 23:11:52 +0800

    202401082311

--- src/Pages/DepthComponent/ComponentModel/ComponentModel.vue
+++ src/Pages/DepthComponent/ComponentModel/ComponentModel.vue
@@ -5,25 +5,28 @@
     <!-- 无论选择哪种写法，确保父组件中的属性名与子组件中的 prop 名称保持一致，以确保正确的双向绑定。
     model-value 是子组件的 prop 名称，而 searchText 是父组件中的属性名 -->
     <h5>方法1：</h5>
-    <input v-model="searchText" />
+    <input v-model="searchText" />{{ searchText }}
     <!-- 模板编译器会对 v-model 进行更冗长的等价展开。因此上面的代码其实等价于下面这段 -->
     <h5>方法2：</h5>
-    <input :value="searchText" @input="searchText = $event.target.value" />
+    <input :value="searchText" @input="searchText = $event.target.value" />{{
+      searchText
+    }}
     <h5>方法3：</h5>
     <!-- 子组件传递的新值赋给父组件中的属性 -->
     <!-- 箭头函数 (newValue) => (searchText = newValue) 可以提供更多的灵活性 -->
     <CustomInput
       :model-value="searchText"
       @update:model-value="(newValue) => (searchText = newValue)"
-    />
+    />{{ searchText }}
     <h5>方法4：</h5>
     <!-- 子组件传递的新值赋给父组件中的属性 -->
     <!-- $event 则是一个简洁的写法，直接将传递的新值赋给属性 -->
     <CustomInput
       :model-value="searchText"
       @update:modelValue="searchText = $event"
-    />
-    <h4>实现了输入框的双向绑定</h4>
+    />{{ searchText }}
+    <h3>实现了输入框的双向绑定</h3>
+    <h5>实现父组件与子组件的双向绑定: {{ message }}</h5>
     <!-- 自动监听 'update:modelValue' 事件 -->
     <!-- 新值 $event 赋给 inputValue 属性 -->
     <!-- 在输入框的值发生变化时触发'update:modelValue' 事件，同时将新的值传递给父组件 -->
@@ -31,7 +34,10 @@
     <CustomInput v-model="message" />{{ message }}
     <h5>方法2：</h5>
     <!-- v-model="message"相当于@update:modelValue="message = $event" -->
-    <CustomInput @update:modelValue="message = $event" />{{ message }}
+    <CustomInput
+      :model-value="message"
+      @update:modelValue="message = $event"
+    />{{ message }}
     <h5>方法3：</h5>
     <CustomInputTwo v-model="message" />{{ message }}
     <h5>方法4：</h5>
